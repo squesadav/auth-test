c0c04110bed77d7072ca4e87e5ad265238e9c827
squesadav <sebas.quesada790@gmail.com>
2021-05-03 15:10:36 -0600

    created Login and Dashboard pages and connected

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 
 export default function App() {
   return (
@@ -14,7 +16,9 @@ export default function App() {
           <Router>
             <AuthProvider>
               <Switch>
+                <Route exact path="/" component={Dashboard}/>
                 <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
               </Switch>
             </AuthProvider>
           </Router>
