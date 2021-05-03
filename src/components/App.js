59d331a07ec053aa40d9f25dc61f565c9b2a7f8e
squesadav <sebas.quesada790@gmail.com>
2021-05-03 16:10:53 -0600

    finished update profile component

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./Dashboard";
 import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import ForgotPassword from "./ForgotPassword"
+import UpdateProfile from "./UpdateProfile";
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
             <AuthProvider>
               <Switch>
                 <PrivateRoute exact path="/" component={Dashboard}/>
+                <PrivateRoute path="/update-profile" component={UpdateProfile}/>
                 <Route path="/signup" component={Signup} />
                 <Route path="/login" component={Login} />
                 <Route path="/forgot-password" component={ForgotPassword} />
