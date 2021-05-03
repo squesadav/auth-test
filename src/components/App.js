47f844cea0995951c8104715b6adfee873890bbd
squesadav <sebas.quesada790@gmail.com>
2021-05-03 14:51:21 -0600

    using auth context in signup and handling submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
+import React from "react";
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
 
 export default function App() {
   return (
-    <Container 
-      className="d-flex align-items-center justify-content-center"
-      style = {{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style = {{ maxWidth: "400px"}}>
-        <Signup />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container 
+        className="d-flex align-items-center justify-content-center"
+        style = {{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style = {{ maxWidth: "400px"}}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   )
 }
