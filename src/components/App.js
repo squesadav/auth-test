d3c7606b5713a879f921497b5768c330e1eceafa
squesadav <sebas.quesada790@gmail.com>
2021-05-02 23:03:16 -0600

    fixed bootstrap styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
-import Signup from "./Signup"
+import Signup from "./Signup";
+import { Container } from "react-bootstrap";
 
 export default function App() {
   return (
-    <Signup />
+    <Container 
+      className="d-flex align-items-center justify-content-center"
+      style = {{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style = {{ maxWidth: "400px"}}>
+        <Signup />
+      </div>
+    </Container>
   )
 }
