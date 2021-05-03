f6fca98c64f2f1f77d5640ecaaa22216c74ded67
squesadav <sebas.quesada790@gmail.com>
2021-05-03 15:02:52 -0600

    started routing in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <AuthProvider>
       <Container 
         className="d-flex align-items-center justify-content-center"
         style = {{ minHeight: "100vh" }}
       >
         <div className="w-100" style = {{ maxWidth: "400px"}}>
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route path="/signup" component={Signup} />
+              </Switch>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
   )
 }
